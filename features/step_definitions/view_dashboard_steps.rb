8dbc83c1257730e43586f648109d408701f1f3d1
JWTappert <tappertj@oregonstate.edu>
2017-03-13 15:27:32 -0700

    Added user dashboard step definitions

---
+++ features/step_definitions/view_dashboard_steps.rb
@@ -0,0 +1,19 @@
+When(/^I visit the user dashboard$/) do
+  pending
+end
+
+Then(/^I should see a heading that reads "Welcome Username"$/) do |text|
+  pending
+end
+
+Given(/^I am on my user dashboard$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I click on the view projects icon$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see a list view of all of my already existing projects$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
