1b700f2fd7849d5c4a7a5a38a911296a349d5cfb
JWTappert <tappertj@oregonstate.edu>
2017-03-13 15:33:38 -0700

    Revised step difinitions again

--- features/step_definitions/view_dashboard_steps.rb
+++ features/step_definitions/view_dashboard_steps.rb
@@ -1,23 +1,19 @@
-Given(/^I am already logged in$/) do
-  pending
-end
-	
-When(/^I visit the user dashboard$/) do
+Given(/^I am logged in and I visit the user dashboard&/) do
   pending
 end
 
-Then(/^I should see a heading that reads "Welcome Username"$/) do |text|
-  pending
-end
-
-Given(/^I am on my user dashboard$/) do
+Then(/^I should see a heading that says, "([^"]*)"\.$/) do |arg1|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-When(/^I click on the view projects icon$/) do
+Given(/^I am on the user dashboard page$/) do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-Then(/^I should see a list view of all of my already existing projects$/) do
+When(/^I click the view all projects icon$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see a list view of the projects I have already created\.$/) do
   pending # Write code here that turns the phrase above into concrete actions
 end
