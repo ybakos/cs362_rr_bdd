32dca8bbf55b93abbcb16081c7dad2280ee03f58
Yong Bakos <ybakos@humanoriented.com>
2017-03-13 15:03:06 -0700

    Add initial land owner scenarios.

---
+++ features/land_owner_create_project.feature
@@ -0,0 +1,19 @@
+Feature: Land Owner creates a restoration project
+  As a land owner
+  I want to create a river restoration project
+  So that I can manage it.
+
+  Scenario: Land Owner visits home page
+    # Given ...
+    When I visit the homepage
+    Then I should see a link to "create a project".
+
+  Scenario: Land Owner creates a new project
+    Given I am on the create project screen
+    When I enter my project details
+    And click the save button
+    And foo
+    And foo
+    And foo
+    Then I should see a confirmation that my project has been created.
+
---
+++ features/step_definitions/create_project_steps.rb
@@ -0,0 +1,23 @@
+When(/^I visit the homepage$/) do
+  visit root_path
+end
+
+Then(/^I should see a link to "([^"]*)"\.$/) do |text|
+  flunk
+end
+
+Given(/^I am on the create project screen$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I enter my project details$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^click the save button$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see a confirmation that my project has been created\.$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
