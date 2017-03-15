706b1271767834dc0e616e9d039c04296908d050
Tyler Radabaugh <radabaughtyler24@gmail.com>
2017-03-15 12:51:09 -0700

    created guest_view_public_project feature and step defs.

---
+++ features/step_definitions/view_public_projects_steps.rb
@@ -0,0 +1,7 @@
+When(/^I am on the home page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see a link to view "([^"]*)"\.$/) do |text|
+    pending
+end
