8cc717f2ae38a655f951d8a3fbf07a878a3de668
Jack Wilson <jack.wilson.east@gmail.com>
2017-03-15 10:24:38 -0700

    changed steps for delete project to avoid ambigious errors

--- features/step_definitions/delete_project_steps.rb
+++ features/step_definitions/delete_project_steps.rb
@@ -1,17 +1,12 @@
-Given(/^I am on the user dashboard page$/) do
-  #get "/"
-  assert true
+Given(/^On the user dashboard page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
 end
 
 When(/^I go to a restoration project I want to remove$/) do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-When(/^I then click "([^"]*)"$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then(/^the restoration project is from the user dashboard page$/) do
+When(/^I click delete$/) do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
