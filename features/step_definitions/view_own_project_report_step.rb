66289c12127d58d2f19c32e685e0bf924e3608e5
zmwilson <zmwilson91@gmail.com>
2017-03-20 00:47:42 -0700

    Create land_owner_view_report step defs

---
+++ features/step_definitions/view_own_project_report_step.rb
@@ -0,0 +1,15 @@
+Given(/^I am on the view my report screen$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I select a project$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^click the view button$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see a report showing changes over time of my project\.$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
